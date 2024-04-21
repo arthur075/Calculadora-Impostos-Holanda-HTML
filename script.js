1833f65cd4b4fedafc15da8079bcee6f04688864
Arthur Matos <matossilvaarthur71@gmail.com>
2024-04-21 15:14:44 -0300

    commit04

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById('select').addEventListener('change', function() {
+document.getElementById('select').addEventListener('change', function () {
     calcularSalarioComImpostos();
 });
 
-document.getElementById('salarioInput').addEventListener('input', function() {
+document.getElementById('salarioInput').addEventListener('input', function () {
     calcularSalarioComImpostos();
 });
 
@@ -10,43 +10,52 @@ function calcularSalarioComImpostos() {
     var salario = parseFloat(document.getElementById('salarioInput').value);
     var tipoSalario = document.getElementById('select').value;
     var salarioComImpostos = 0;
+    var imposto = 0;
 
     // Realizar cálculos com base no tipo de salário selecionado
     switch (tipoSalario) {
         case 'Diário':
             // Lógica para cálculo diário
-            salarioComImpostos = salario * 365;
+            salarioComImpostos = salario * 30 * 12;
             if (salarioComImpostos <= 75518) {
-                salarioComImpostos = (salarioComImpostos * 36.93) / 100;
+                imposto = (salarioComImpostos * 36.93) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             } else if (salarioComImpostos > 75518) {
-                    salarioComImpostos = (salarioComImpostos * 49.50) / 100;
+                imposto = (salarioComImpostos * 49.50) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             }
             break;
         case 'Semanal':
             // Lógica para cálculo semanal
             salarioComImpostos = salario * 4 * 12;
             if (salarioComImpostos <= 75518) {
-                salarioComImpostos = (salarioComImpostos * 36.93) / 100;
+                imposto = (salarioComImpostos * 36.93) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             } else if (salarioComImpostos > 75518) {
-                    salarioComImpostos = (salarioComImpostos * 49.50) / 100;
+                imposto = (salarioComImpostos * 49.50) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             }
             break;
         case 'Mensal':
             // Lógica para cálculo mensal
             salarioComImpostos = salario * 12;
             if (salarioComImpostos <= 75518) {
-                salarioComImpostos = (salarioComImpostos * 36.93) / 100;
+                imposto = (salarioComImpostos * 36.93) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             } else if (salarioComImpostos > 75518) {
-                    salarioComImpostos = (salarioComImpostos * 49.50) / 100;
+                imposto = (salarioComImpostos * 49.50) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             }
             break;
         case 'Anual':
             // Lógica para cálculo anual
             salarioComImpostos = salario * 1;
             if (salarioComImpostos <= 75518) {
-                salarioComImpostos = (salarioComImpostos * 36.93) / 100;
+                imposto = (salarioComImpostos * 36.93) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             } else if (salarioComImpostos > 75518) {
-                    salarioComImpostos = (salarioComImpostos * 49.50) / 100;
+                imposto = (salarioComImpostos * 49.50) / 100;
+                salarioComImpostos = salarioComImpostos - imposto;
             }
             break;
         default:
